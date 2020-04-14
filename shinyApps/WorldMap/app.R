39e6b971f374371438a720115bd4a9dd51c01765
MarekOo <moseswillrein@gmx.de>
2020-04-14 19:54:49 +0200

    none

--- shinyApps/WorldMap/app.R
+++ shinyApps/WorldMap/app.R
@@ -15,12 +15,6 @@ theme_set(
 )
 #setwd("~/ShinyApps/worldmap")
 
-#subject = "Deaths"
-
-#Cases <- read.csv(paste0("data_aggregated_Cases.csv"),row.names = NULL)
-#Deaths <- read.csv(paste0("data_aggregated_Deaths.csv"),row.names = NULL)
-#Recovered <- read.csv(paste0("data_aggregated_Recovered.csv"),row.names = NULL)
-
 #Cases
 load("data_aggregated_Cases.RData")
 #Deaths
@@ -28,11 +22,15 @@ load("data_aggregated_Deaths.RData")
 #Recovered
 load("data_aggregated_Recovered.RData")
 
+#Cases
+load("data_aggregated_Cases_daily.RData")
+#Deaths
+load("data_aggregated_Deaths_daily.RData")
+#Recovered
+load("data_aggregated_Recovered_daily.RData")
 
 #Load the World Map from the maps package
 world_map <- map_data("world")
-
-#setwd("~/ShinyApps/sample-apps/covid19_world_map")
 
 allDates <- Cases$date
 allCountries <- sort(unique(Cases$region))
@@ -51,7 +49,7 @@ ui <- fluidPage(
   
   # App title ----
   titlePanel("COVID-19 Outbreak World Map"),
-  
+  tags$h4("The graph shows the outbreak numbers on a map, having the regions colored by the intensity of the case numbers."),
   # Sidebar layout with input and output definitions ----
   
   fluidRow(
@@ -70,9 +68,11 @@ ui <- fluidPage(
              plotOutput(outputId = "plotA")
       )
     ),
+  
   ####################################################################
   hr(),
-  titlePanel("COVID-19 Outbreak World Map"),
+  titlePanel("COVID-19 Outbreak Graph over Time"),
+  tags$h4("The graph shows the outbreak timeline. Choose countries and a time range from the menu"),
   fluidRow(
     column(4,
            selectInput(inputId = "countries",
@@ -89,6 +89,9 @@ ui <- fluidPage(
            plotOutput(outputId = "plotB")
     )
   ),
+  hr(),
+  titlePanel("COVID-19 weekly developlemt"),
+  tags$h4("The graph shows the development to assess a turning point, when the country falls below the diagonal line. Choose countries and a time from the menu"),
   fluidRow(
     column(4,
            sliderInput(inputId = "day2",
@@ -96,9 +99,14 @@ ui <- fluidPage(
                        min = min(days),
                        max = max(days),
                        value = max(days)),
-           selectInput(inputId = "subject",
+           selectInput(inputId = "subject2",
                        label = "Choose a dataset:",
-                       choices = c("Cases", "Deaths", "Recovered"))
+                       choices = c("Cases", "Deaths", "Recovered")),
+           selectInput(inputId = "countries2",
+                       label = "Countries",
+                       multiple = TRUE,
+                       choices = allCountries,
+                       selected = c("USA","Germany","China"))
            
     ),
     column(6,
@@ -167,7 +175,65 @@ server <- function(input, output) {
   })
   
   output$plotC <- renderPlot({
-    plot(NULL)
+    
+    if(input$subject2 == "Cases"){
+      data_test <- Cases_daily
+      subject = "Infected Cases"
+    }else if(input$subject2 == "Deaths"){
+      data_test <- Deaths_daily
+      subject = "Deaths"
+    }else{
+      data_test <- Recovered_daily
+      subject = "Recovered Cases"
+    }
+    needle = input$countries2
+    data_test = data_test[which(data_test$region %in% needle),]
+    
+    countries = unique(data_test$region)
+    minDate = min(data_test$date)
+    data_test$weeklyCases = NA
+    data_test$dailyCases = data_test$cases
+    data_test$cases = NA
+    
+    for (i in 1:length(countries)){
+      item = countries[i]
+      print(item)
+      for (j in 1:(length(unique(data_test$date))-7)) {
+        print(j)
+        startDate = minDate-1+j
+        EndDate = startDate+7
+        sumWeek = sum(data_test[which(data_test$date<EndDate & data_test$date>=startDate & data_test$region==item),]$dailyCases)
+        totalCases = sum(data_test[which(data_test$date<EndDate & data_test$region==item),]$dailyCases)
+        data_test[which(data_test$date==EndDate & data_test$region==item),]$weeklyCases = sumWeek
+        data_test[which(data_test$date==EndDate & data_test$region==item),]$cases = totalCases
+      }
+      
+    }
+    
+    data_test <- data_test[data_test$date >= (minDate+7),]
+    
+    maxDate = minDate+6+j
+    maxDate = input$day2
+    
+    print(maxDate)
+    data_plot <- data_test[data_test$date <= maxDate,]
+    data_point <- data_test[data_test$date == maxDate,]
+    plotC <- ggplot(data_plot, aes(y=weeklyCases,x=cases,colour=region)) + 
+      geom_line(size=0.8,alpha=0.3) + 
+      geom_abline(intercept = 0, slope = 1, color="white",linetype="dotted")+
+      geom_point(data=data_point,aes(y=weeklyCases,x=cases,colour=region),size=3) +
+      geom_text(data = data_point, aes(y=weeklyCases,x=cases, label = region), size = 4, vjust = 2.5) +
+      scale_x_log10(labels = scales::comma, limits=c(1,1000000),breaks=c(10,100,1000,10000,100000,1000000)) + 
+      scale_y_log10(labels = scales::comma, limits=c(1,1000000),breaks=c(10,100,1000,10000,100000,1000000)) + 
+      ggtitle(paste0("CVOID-19 ",subject," per Week vs Total Cases on: ",maxDate)) + xlab(paste0("Total COVID-19 ",subject)) + 
+      ylab("COVID-19 Cases last 7 Days")+
+      scale_color_brewer(palette="Spectral") + 
+      dark_theme_gray() +
+      theme(axis.text.x = element_text(size = 12,color="grey")) +
+      theme(axis.text.y = element_text(size = 12,color="grey")) +
+      theme(plot.title = element_text(size=15,color="orange")) +
+      theme(legend.position = "none") 
+    plot(plotC)
   })
   
 }
--- shinyApps/WorldMap/preLoad.R
+++ shinyApps/WorldMap/preLoad.R
@@ -28,6 +28,7 @@ for (subject in subjectList) {
   data$Long = NULL
   #check out the data frame 
   str(data)
+  data_daily <- data
   
   # using the melt function to transpose the data frame (needs prior transformation to data table format)
   data_transformed <- melt(as.data.table(data), id=c("Province.State","Country.Region"), fun="Sum")
@@ -51,6 +52,41 @@ for (subject in subjectList) {
   write.csv(data_aggregated,paste0("data_aggregated_",subject,".csv"))
   assign(subject, data_aggregated)
   save(list=c(subject),data_aggregated,file = paste0("data_aggregated_",subject,".RData"))
+  
+  # Daily Cases
+  
+  # check how many non date fields you have + 2 !!!
+  first_cases_fields = 3 + 1
+  
+  max = length(data)
+  
+  # to calculate the daily cases, we simply substract the today vs yesterdays totals
+  # sometimes the data is faulty, that's when you get negative cases :/
+  for (i in max:first_cases_fields) {
+    data_daily[,i] = data_daily[,i] - data_daily[,(i-1)]
+    #due to meassure errors we get negative values sometimes ... 
+    data_daily[which(data_daily[,i]<0),i]=0
+  }
+  
+  # using the melt function to transpose the data frame (needs prior transformation to data table format)
+  data_transformed_daily <- melt(as.data.table(data_daily), id=c("Province.State","Country.Region"), fun="Sum")
+  # change the variable names to something more "speaking"
+  names(data_transformed_daily) <-  c("region","country","date","cases")
+  # remove the X invoked by R (no variable names beginning with a number allowed)
+  data_transformed_daily$date <- gsub("X", "0", paste(data_transformed_daily$date))
+  # transform character dates to actual date format
+  data_transformed_daily$date <- as.Date(data_transformed_daily$date, "%m.%d.%Y")
+  
+  # Now aggregate the covid data frame by region and date -> number of cases per region per date
+  data_aggregated_daily <- aggregate(cases ~ country + date, data = data_transformed_daily, sum)
+  names(data_aggregated_daily) = c("region","date","cases")
+  
+  # unfortunately the CSSEGISandData dataset contains some non-standard names for countries. These have to be fixed manually :/
+  data_aggregated_daily$region[which(data_aggregated_daily$region=="US")] = "USA"
+  data_aggregated_daily$region[which(data_aggregated_daily$region=="Taiwan*")] = "Taiwan"
+  write.csv(data_aggregated,paste0("data_aggregated_",subject,"_daily.csv"))
+  assign(paste0(subject,"_daily"), data_aggregated_daily)
+  save(list=c(paste0(subject,"_daily")),data_aggregated_daily,file = paste0("data_aggregated_",subject,"_daily.RData"))
 }
 
 
